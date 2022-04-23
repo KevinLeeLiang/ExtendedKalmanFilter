cd899ffd9177b287216b568b3f194c04e5e862e9
lijialiang <lijialiang362>
2022-04-23 18:09:57 +0800

    提交正弦曲线代码“

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
+# https://blog.csdn.net/qq_27806947/article/details/106315337
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
